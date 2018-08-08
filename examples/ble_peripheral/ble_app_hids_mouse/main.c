1016cb5d832ac3c085e3a395d7604007d837fe29
chaoqun.wang <chaoqun.wang@ivglass.com>
2018-08-08 18:54:56 +0800

    SW_K02_YC_ActionOne_A_V221_180808 wangcq327_v221_K02

--- examples/ble_peripheral/ble_app_hids_mouse/main.c
+++ examples/ble_peripheral/ble_app_hids_mouse/main.c
@@ -295,8 +295,8 @@ MODE_DISCONNECT,
 //###################################
 //-----------you work here -------------
 //#define PROJECT_HaloMini
-#define PROJECT_K02
-//#define PROJECT_K07
+//#define PROJECT_K02
+#define PROJECT_K07
 
 #ifdef PROJECT_HaloMini
 static enum Mode_select MODE_INIT = MODE_2D;
@@ -321,7 +321,7 @@ char project_flag=0x02;
 static enum Mode_select MODE_INIT = MODE_3D;   // the init mode of connection
 static bool report_system_in_3D_mode = false;  // if use the function of transfer key to system in 3D mode
 char project_flag=0x03;
-#define MANUFACTURER_NAME               "wangcq327_v220_K07"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
+#define MANUFACTURER_NAME               "wangcq327_v221_K07"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
 #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(8, UNIT_1_25_MS)             /**< Maximum connection interval (15 ms). */
 //#define TRANSFER_FORMAT_1
 #endif
@@ -3846,6 +3846,23 @@ void sensor_data_poll_handler(void* p_context)
 		touchBackKeyTime=0;
 		set_data(KEY_EARSE_BONDS,0x00);
 	}
+#ifdef PROJECT_K07
+	static int powerKeyTime = 0;
+	if(bsp_button_is_pressed(2) == 1 ){
+		powerKeyTime++;
+		if((powerKeyTime * SENSOR_POLL_INTERVAL) > 4000  ){//5s
+			powerKeyTime = 0;
+			NRF_LOG_INFO("----- ------------- power key 5s sleep\r\n");
+			sw3153_light_select(RED, BLINK_LEVEL_NON);
+			nrf_delay_ms(2000);
+			sleep_mode_enter_power();
+		}
+	}else{
+		//sw3153_light_select(GREEN, BLINK_LEVEL_2);
+		powerKeyTime = 0;
+	}
+#endif
+
 #if 0
 	//get_data(TIME_STAMP);
 	//get_data(PACKET_ID);
