975287924daab9eed52f0b89dd412fac599fcbad
chaoqun.wang <chaoqun.wang@ivglass.com>
2018-08-09 11:58:47 +0800

    SW_K02_YC_ActionOne_A_V222_180809 wangcq327_v222_K02

--- examples/ble_peripheral/ble_app_hids_mouse/main.c
+++ examples/ble_peripheral/ble_app_hids_mouse/main.c
@@ -295,8 +295,8 @@ MODE_DISCONNECT,
 //###################################
 //-----------you work here -------------
 //#define PROJECT_HaloMini
-//#define PROJECT_K02
-#define PROJECT_K07
+#define PROJECT_K02
+//#define PROJECT_K07
 
 #ifdef PROJECT_HaloMini
 static enum Mode_select MODE_INIT = MODE_2D;
@@ -311,7 +311,7 @@ char project_flag=0x01;
 static enum Mode_select MODE_INIT = MODE_3D;   // the init mode of connection
 static bool report_system_in_3D_mode = false;  // if use the function of transfer key to system in 3D mode
 char project_flag=0x02;
-#define MANUFACTURER_NAME               "wangcq327_v220_K02"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
+#define MANUFACTURER_NAME               "wangcq327_v222_K02"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
 #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(8, UNIT_1_25_MS)             /**< Maximum connection interval (15 ms). */
 //#define TRANSFER_FORMAT_1
 #endif
