078a50392d01ea6f8af788c570a2646553c99cfe
chaoqun.wang <chaoqun.wang@ivglass.com>
2018-09-19 18:27:56 +0800

    K07 230 / K02 228

--- examples/ble_peripheral/ble_app_hids_mouse/main.c
+++ examples/ble_peripheral/ble_app_hids_mouse/main.c
@@ -297,8 +297,8 @@ MODE_DISCONNECT,
 //###################################
 //-----------you work here -------------
 //#define PROJECT_HaloMini
-#define PROJECT_K02
-//#define PROJECT_K07
+//#define PROJECT_K02
+#define PROJECT_K07
 
 #ifdef PROJECT_HaloMini
 static enum Mode_select MODE_INIT = MODE_2D;
@@ -313,7 +313,7 @@ char project_flag=0x01;
 static enum Mode_select MODE_INIT = MODE_3D;   // the init mode of connection
 static bool report_system_in_3D_mode = false;  // if use the function of transfer key to system in 3D mode
 char project_flag=0x02;
-#define MANUFACTURER_NAME               "wangcq327_v227_K02"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
+#define MANUFACTURER_NAME               "wangcq327_v228_K02"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
 #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(8, UNIT_1_25_MS)             /**< Maximum connection interval (15 ms). */
 //#define TRANSFER_FORMAT_1
 #endif
@@ -323,7 +323,7 @@ char project_flag=0x02;
 static enum Mode_select MODE_INIT = MODE_3D;   // the init mode of connection
 static bool report_system_in_3D_mode = false;  // if use the function of transfer key to system in 3D mode
 char project_flag=0x03;
-#define MANUFACTURER_NAME               "wangcq327_v229_K07"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
+#define MANUFACTURER_NAME               "wangcq327_v230_K07"  //vX.X.X  0<=X<=9  the max version is wangcq327_v9.9.9_...                     /**< Manufacturer. Will be passed to Device Information Service. */
 #define MAX_CONN_INTERVAL               MSEC_TO_UNITS(8, UNIT_1_25_MS)             /**< Maximum connection interval (15 ms). */
 //#define TRANSFER_FORMAT_1
 #endif
@@ -2379,10 +2379,15 @@ static void advertising_init(void)
     uint8_t                adv_flags;
     ble_advdata_t          advdata;
     ble_adv_modes_config_t options;
-
+	ble_advdata_manuf_data_t manuf;
     // Build and set advertising data
     memset(&advdata, 0, sizeof(advdata));
 
+	manuf.company_identifier = 0x8888;
+	manuf.data.size = 2;//strlen(DEVICE_NAME);
+	manuf.data.p_data = "SC";
+	
+	advdata.p_manuf_specific_data = &manuf;
     adv_flags                       = BLE_GAP_ADV_FLAGS_LE_ONLY_LIMITED_DISC_MODE;
     advdata.name_type               = BLE_ADVDATA_FULL_NAME;
     advdata.include_appearance      = true;
